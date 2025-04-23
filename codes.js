1849e7472d3030a165a9ac0c3d69eb959aeaf8ba
buyschoolbot <scamer69420@hotmail.com>
2025-04-23 17:56:48 -0400

    more codes added

---
+++ codes.js
@@ -0,0 +1,8 @@
+// Only you should edit this file to manage secret codes and iframe links
+const iframeCodes = {
+    "9032": "https://deadshot.io",
+    // Add more like this:
+    // "1234": "https://blocked.goguardian.com",
+    // "7452": "https://abacademy.github.io"
+  };
+  
