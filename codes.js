7a6100bcea4af74f0d62466bc301213a648d9fed
buyschoolbot <scamer69420@hotmail.com>
2025-04-24 15:58:30 -0400

    rfyi7

--- codes.js
+++ codes.js
@@ -1,7 +1,7 @@
 const iframeCodes = {
     "9032": "https://www.deadshot.io", // The code for Deadshot.io
     // Add new codes like this:
-     "1234": "1v1.lol",
+     "8433": "1v1.lol",
      "5678": "abacademy.github.io",
   };
   
