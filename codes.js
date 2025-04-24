71968e6e34280c3fcb6f1585b4b5f50e499440a4
buyschoolbot <scamer69420@hotmail.com>
2025-04-23 20:12:00 -0400

    something went wrong... jk it works

--- codes.js
+++ codes.js
@@ -1,7 +1,7 @@
 const iframeCodes = {
     "9032": "https://www.deadshot.io", // The code for Deadshot.io
     // Add new codes like this:
-    // "1234": "www.youtube.com",
-    // "5678": "abacademy.github.io",
+     "1234": "www.youtube.com",
+     "5678": "abacademy.github.io",
   };
   
