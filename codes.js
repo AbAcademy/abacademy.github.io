07979ddab6c6f54a37b6220b625efdebd36967a4
buyschoolbot <scamer69420@hotmail.com>
2025-04-24 20:49:37 -0400

    I don't want to specify

--- codes.js
+++ codes.js
@@ -1,5 +1,5 @@
 const iframeCodes = {
-    "9032": "https://www.deadshot.io", "8433": "1v1.lol",
+    "9032": "https://www.deadshot.io", "8433": "krunker.io",
     // Add new codes like this:
      "5678": "abacademy.github.io",
   };
